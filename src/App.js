1aadc4b1379f976304933c76a9a71a9983f24100
= <liam2robertson@gmail.com>
2021-01-19 19:13:04 +0000

    Use router correctly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import HomePage from './components/HomePage';
 import StorePage from './components/StorePage';
+import ItemPage from './components/ItemPage';
 import CheckoutPage from './components/CheckoutPage';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -11,17 +12,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-
-          <Route exact path="/products">
-            <StorePage />
-          </Route>
-
-          <Route exact path="/checkout">
-            <CheckoutPage />
-          </Route>
+          <Route exact path="/" component={HomePage}/>
+          <Route exact path="/products" component={StorePage}/>
+          <Route exact path="/products/:item" component={ItemPage}/>
+          <Route exact path="/checkout" component={CheckoutPage}/>
         </Switch>
       </Router>
     </div>
