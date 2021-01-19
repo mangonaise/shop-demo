0b36511cd2937cbb1603254c76d30ce1aba21f4b
= <liam2robertson@gmail.com>
2021-01-19 17:33:17 +0000

    Create router outline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-
 import './App.css';
+import HomePage from './components/HomePage';
+import StorePage from './components/StorePage';
+import CheckoutPage from './components/CheckoutPage';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+
+          <Route exact path="/products">
+            <StorePage />
+          </Route>
+
+          <Route exact path="/checkout">
+            <CheckoutPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
