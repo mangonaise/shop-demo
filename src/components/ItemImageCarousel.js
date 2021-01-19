832e15b0c3ca0fc0ad6dbd7e18d6ed44f55607f4
= <liam2robertson@gmail.com>
2021-01-19 22:15:33 +0000

    Rework image carousel

--- src/components/ItemImageCarousel.js
+++ src/components/ItemImageCarousel.js
@@ -1,26 +1,39 @@
 import React, { useState } from 'react';
+import './styles/ItemImageCarousel.css';
 
 const ItemImageCarousel = ({ images }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  function cycleImage() {
+  function showNextImage() {
     setCurrentImageIndex(prevIndex => {
       return prevIndex + 1 === images.length ? 0 : prevIndex + 1;
     })
   }
 
+  function showPreviousImage() {
+    setCurrentImageIndex(prevIndex => {
+      return prevIndex -1 === -1 ? images.length - 1 : prevIndex - 1;
+    })
+  }
+
   return (
-    <div id="item-page-image-container">
+    <div className="image-carousel">
+      <div className="carousel-selection">
+        <p>{`${currentImageIndex + 1} of ${images.length}`}</p>
+        <button onClick={showPreviousImage}>{"⯇"}</button>
+        <button onClick={showNextImage}>{"⯈"}</button>
+      </div>
+      
       <img 
         src={images[currentImageIndex]} 
         alt="product"
-        onClick={cycleImage}
+        onClick={showNextImage}
       />
-      <div id="item-page-image-selection">
+      <div>
         {images.map((_, index) => (
-          <button 
+          <button
             key={index}
-            className={index === currentImageIndex ? "selected-image-button" : ""}
+            className={(index === currentImageIndex ? "carousel-selected" : "") + " carousel-selection-indicator"}
             onClick={() => setCurrentImageIndex(index)}>
           </button>
         ))}
