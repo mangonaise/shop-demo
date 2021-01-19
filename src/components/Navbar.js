b5f50f518f5ffa8d808145856bdf1f6f5a433ada
= <liam2robertson@gmail.com>
2021-01-19 17:32:56 +0000

    Create navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles/Navbar.css';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+  return (
+    <nav id="navbar">
+      <Link to="/" id="navbar-title" className="navbar-item">jqk.</Link>
+      <NavLink to="/home" className="navbar-item">Home</NavLink>
+      <NavLink to="/products" className="navbar-item">Store</NavLink>
+      <NavLink to="/checkout" id="basket-link" className="navbar-item">
+        <FontAwesomeIcon icon={faShoppingCart} size="lg"/>
+      </NavLink>
+    </nav>
+  )
+}
+
+export default Navbar;
